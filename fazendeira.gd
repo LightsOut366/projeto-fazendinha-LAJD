63bcc79b6b67f1d7410824372e25e8f35043fa0e
mileenacm <vibesstudy2@gmail.com>
2026-04-15 10:06:07 -0300

    Movimentação
    Fiz a movimentação top-down, faltam frames de animação de up and down

---
+++ fazendeira.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -100.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	#movimentar para os 4 lados
+	var direction = Input.get_vector("Esquerda", "Direita", "UP","Down")
+	
+	if Input.is_action_pressed("Direita") || Input.is_action_pressed("Esquerda"):
+		direction.y=0
+		
+	elif Input.is_action_pressed("UP") || Input.is_action_pressed("Down"):
+		direction.x=0
+	
+	else:
+		direction = Vector2.ZERO
+		$AnimatedSprite2D.play("parado") # sem apertar botao, fica parado
+		
+	direction = direction.normalized()
+	
+	velocity = (direction * SPEED)
+	move_and_slide()
+	
+	
+	# Andar para frente e para trás
+	if Input.is_action_just_pressed("ui_left"):
+		$AnimatedSprite2D.play("andando_tras")
+		
+	if Input.is_action_just_pressed("ui_right"):
+		$AnimatedSprite2D.play("andando_frente")
