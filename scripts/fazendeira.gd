dfffa448db5db8f7dad7dd947f8895c733127d2c
mileenacm <vibesstudy2@gmail.com>
2026-04-26 09:23:16 -0300

    Colisão na casinha, cofre e baldes

--- scripts/fazendeira.gd
+++ scripts/fazendeira.gd
@@ -35,7 +35,9 @@ func _physics_process(delta) -> void:
 	position += direction * delta # move o personagem sem atrelar ao fps
 	position = position.clamp(Vector2.ZERO, tam_tela) # nao deixa ele sair da tela
 	
+	
 	if Input.is_action_pressed("interagir"):
 		pass
 		# botar aqui as animaçoes dela regando
 	
+	move_and_collide(velocity * delta)
